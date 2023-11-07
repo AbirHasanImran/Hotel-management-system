41aec2df7b06f430b654bf8b77a9fb406948bf46
Abir <96690346+AbirHasanImran@users.noreply.github.com>
2023-11-07 14:46:04 +0600

    Add files via upload

---
+++ db.php
@@ -0,0 +1,4 @@
+<?php
+$con = mysqli_connect("localhost","root","","ocean") or die(mysql_error());
+
+?>
---
+++ reservation.php
@@ -0,0 +1,274 @@
+<?php
+include('db.php')
+?>
+<!DOCTYPE html>
+<html >
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>RESERVATION Ocean Paradise</title>
+	<!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+     <!-- Google Fonts-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a  href="../index.php"><i class="fa fa-home"></i> Homepage</a>
+                    </li>
+                    
+					</ul>
+
+            </div>
+
+        </nav>
+       
+        <div id="page-wrapper" >
+            <div id="page-inner">
+			 <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                            RESERVATION <small></small>
+                        </h1>
+                    </div>
+                </div> 
+                                
+            <div class="row">
+                
+                <div class="col-md-5 col-sm-5">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            PERSONAL INFORMATION
+                        </div>
+                        <div class="panel-body">
+						<form name="form" method="post">
+                            <div class="form-group">
+                                            <label>Title*</label>
+                                            <select name="title" class="form-control" required >
+												<option value selected ></option>
+                                                <option value="Dr.">Dr.</option>
+                                                <option value="Miss.">Miss.</option>
+                                                <option value="Mr.">Mr.</option>
+                                                <option value="Mrs.">Mrs.</option>
+												<option value="Prof.">Prof.</option>
+												<option value="Rev .">Rev .</option>
+												<option value="Rev . Fr">Rev . Fr .</option>
+                                            </select>
+                              </div>
+							  <div class="form-group">
+                                            <label>First Name</label>
+                                            <input name="fname" class="form-control" required>
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Last Name</label>
+                                            <input name="lname" class="form-control" required>
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Email</label>
+                                            <input name="email" type="email" class="form-control" required>
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Nationality*</label>
+                                            <label class="radio-inline">
+                                                <input type="radio" name="nation"  value="Bangladesh" checked="">Bangladesh
+                                            </label>
+                                            <label class="radio-inline">
+                                                <input type="radio" name="nation"  value="Non Bangladesh ">Non Bangladesh 
+                                            </label>
+                         
+                                </div>
+						
+                                               
+
+                                    <div class="form-group">
+                                                <label>Passport Country*</label>
+                                            <input name="country" class="form-control" required>
+                                            
+                                           
+								</div>
+								<div class="form-group">
+                                            <label>Phone Number</label>
+                                            <input name="phone" type ="text" class="form-control" required>
+                                            
+                               </div>
+							   
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                  
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            RESERVATION INFORMATION
+                        </div>
+                        <div class="panel-body">
+								<div class="form-group">
+                                            <label>Type Of Room *</label>
+                                            <select name="troom"  class="form-control" required>
+												<option value selected ></option>
+                                                <option value="Superior Room">SUPERIOR ROOM</option>
+                                                <option value="Deluxe Room">DELUXE ROOM</option>
+												<option value="Guest House">GUEST HOUSE</option>
+												<option value="Single Room">SINGLE ROOM</option>
+                                            </select>
+                              </div>
+							  <div class="form-group">
+                                            <label>Bedding Type</label>
+                                            <select name="bed" class="form-control" required>
+												<option value selected ></option>
+                                                <option value="Single">Single</option>
+                                                <option value="Double">Double</option>
+												<option value="Triple">Triple</option>
+                                                <option value="Quad">Quad</option>
+												<option value="None">None</option>
+                                                
+                                             
+                                            </select>
+                              </div>
+							  <div class="form-group">
+                                            <label>No.of Rooms *</label>
+                                            <select name="nroom" class="form-control" required>
+												<option value selected ></option>
+                                                <option value="1">1</option>
+                                          
+                                            </select>
+                              </div>
+							 
+							 
+							  <div class="form-group">
+                                            <label>Meal Plan</label>
+                                            <select name="meal" class="form-control"required>
+												<option value selected ></option>
+                                                <option value="Room only">Room only</option>
+                                                <option value="Breakfast">Breakfast</option>
+												<option value="Half Board">Half Board</option>
+												<option value="Full Board">Full Board</option>
+												
+                                                
+                                             
+                                            </select>
+                              </div>
+							  <div class="form-group">
+                                            <label>Check-In *</label>
+                                            <input name="cin" type ="date" class="form-control">
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Check-Out</label>
+                                            <input name="cout" type ="date" class="form-control">
+                                            
+                               </div>
+                       </div>
+                        
+                    </div>
+                </div>
+				
+				
+                <div class="col-md-12 col-sm-12">
+             
+						<input type="submit" name="submit" class="btn btn-primary">
+
+
+                      
+
+
+
+<?php
+include('db.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $errors = array();
+
+    // Validate Title
+    if (empty($_POST["title"])) {
+        $errors[] = "Title is required";
+    }
+
+    // Validate First Name
+    if (empty($_POST["fname"])) {
+        $errors[] = "First Name is required";
+    }
+
+    // Validate Last Name
+    if (empty($_POST["lname"])) {
+        $errors[] = "Last Name is required";
+    }
+
+    // Validate Email
+    if (empty($_POST["email"])) {
+        $errors[] = "Email is required";
+    } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email format";
+    }
+
+    // Validate Nationality
+    if (empty($_POST["nation"])) {
+        $errors[] = "Nationality is required";
+    }
+
+    // Validate Passport Country
+    if (empty($_POST["country"])) {
+        $errors[] = "Passport Country is required";
+    }
+
+    // Validate Phone Number
+    if (empty($_POST["phone"])) {
+        $errors[] = "Phone Number is required";
+    }
+
+    // If there are no errors, proceed with database insertion
+    if (empty($errors)) {
+        $new ="Not Conform";
+        $newUser="INSERT INTO `roombook`(`Title`, `FName`, `LName`, `Email`, `National`, `Country`, `Phone`, `TRoom`, `Bed`, `NRoom`, `Meal`, `cin`, `cout`,`stat`,`nodays`) VALUES ('$_POST[title]','$_POST[fname]','$_POST[lname]','$_POST[email]','$_POST[nation]','$_POST[country]','$_POST[phone]','$_POST[troom]','$_POST[bed]','$_POST[nroom]','$_POST[meal]','$_POST[cin]','$_POST[cout]','$new',datediff('$_POST[cout]','$_POST[cin]'))";
+        
+        if (mysqli_query($con,$newUser)) {
+            echo "<script type='text/javascript'> alert('Your Booking application has been sent')</script>";
+        } else {
+            echo "<script type='text/javascript'> alert('Error adding user in database')</script>";
+        }
+    } else {
+        // Print error messages
+        foreach ($errors as $error) {
+            echo "<div class='alert alert-danger'>$error</div>";
+        }
+    }
+}
+?>
+
+						</form>
+							
+                    </div>
+                </div>
+            </div>
+           
+                
+                </div>
+                    
+            
+				
+					</div>
+			 <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+   
+   
+</body>
+</html>
